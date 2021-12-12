78f51d555ff5065a7568065f93df25f11862ad71
sebjvidal <seb.vidal@btinternet.com>
2021-12-12 19:41:44 +0000

    Initial commit

---
+++ 100 Days of Swift Project 10/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  100 Days of Swift Project 10
+//
+//  Created by Seb Vidal on 07/12/2021.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+}
